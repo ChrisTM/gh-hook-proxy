522ea842307f57138cb57f811d2b41633f4af015
Chris Mitchell <chris@cloudboltsoftware.com>
2013-09-06 18:09:10 -0700

    update test_payload.py to work with the GitHub Jenkins plugin

--- test_payload.py
+++ test_payload.py
@@ -14,26 +14,31 @@ def craft_payload_data(repo_owner='ChrisTM', repo_name='gh-hook-proxy'):
     Return a dictionary with sample GitHub hook payload data for the repository
     specified by ``repo_owner`` and ``repo_name``.
     """
+    repo_owner_email = '{}@example.com'.format(repo_owner)
+    user_obj = {
+        'name': repo_owner,
+        'email': repo_owner_email
+    }
+
     # Payload modelled after the template shown here:
     # https://help.github.com/articles/post-receive-hooks#the-payload
     return {
         'before': '20ceddf69021a0b1bac73587df73f726e87a5945',
         'after': '871aa44e212e34f7a80853687e4d104f1f9d54ea',
         'ref': "refs/heads/master",
+        # Jenkins' GitHub plugin expects ``pusher``. See https://github.com/jenkinsci/github-plugin/blob/0604bacbf51cf3af7400b4956ecf3a57a6024d29/src/main/java/com/cloudbees/jenkins/GitHubWebHook.java#L162
+        'pusher': user_obj,
         'commits': [
             #{
-            #    'id': commit.id,
-            #    'message': commit.message,
-            #    'timestamp': commit.committed_date.xmlschema,
-            #    'url': commit_url,
-            #    'added': array_of_added_paths,
-            #    'removed': array_of_removed_paths,
-            #    'modified': array_of_modified_paths,
-            #    'author': {
-            #        'name': commit.author.name,
-            #        'email': commit.author.email
-            #    }
-            #}
+            #    'id': '196d59506634023e47f581bc3b2d4a462ec46466',
+            #    'message': 'This is a commit message.',
+            #    'timestamp': '2013-08-02T11:13:17-07:00',
+            #    'url': '',
+            #    'added': [],
+            #    'removed': [],
+            #    'modified': [],
+            #    'author': user_obj,
+            #},
         ],
         'repository': {
             'name': repo_name,
@@ -43,11 +48,8 @@ def craft_payload_data(repo_owner='ChrisTM', repo_name='gh-hook-proxy'):
             'watchers': 0,
             'forks': 0,
             'private': False,
-            'owner': {
-                'name': repo_owner,
-                'email': '{}@example.com'.format(repo_owner)
-            }
-        }
+            'owner': user_obj,
+        },
     }
 
 
@@ -56,7 +58,8 @@ def post_hook(url, payload_data):
     POST a GitHub hook request to ``url`` containing the payload data in
     ``payload_data`` and return a ``requests`` response object.
     """
-    return requests.post(url, data={'payload': json.dumps(payload_data)})
+    payload_json = json.dumps(payload_data)
+    return requests.post(url, data={'payload': payload_json}, verify=False)
 
 
 def create_parser():
@@ -89,5 +92,7 @@ if __name__ == '__main__':
 
     payload_data = craft_payload_data(
         repo_owner=args.repo_owner, repo_name=args.repo_name)
-    r = post_hook(args.url, payload_data)
-    print r.text
+
+    response = post_hook(args.url, payload_data)
+    text = response.text.encode('utf-8')
+    print text
